5b411b4f2f792887f95d7a81e56f435b09646efa
Evan Cendekia <evan.cendekia@gmail.com>
2025-07-19 09:07:56 +0700

    fix meeting1 main.py

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ pygame.display.set_caption('Snake Game')
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 direction = 'RIGHT'
 change_to = direction
+
+snake_pos = [100,50]
+snake_body = [[100,50],[90,50],[80,50]]
+apple_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+apple_spawn = True
+
 white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
 red = pygame.Color(255,0,0)
 green = pygame.Color(0,255,0)
 blue = pygame.Color(0,0,255)
-apple_spawn = False
 
 while True:
     for event in pygame.event.get():
@@ -32,20 +37,17 @@ while True:
                 change_to = 'RIGHT'
             if event.key == pygame.K_ESCAPE:
                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
     game_window.fill(white)
     print(change_to)
-    snake_pos = [100,50]
-    snake_body = [[100,50],[90,50],[80,50]]
+
     snake_body.insert(0, list(snake_pos))
-    
-    #code for snake body
     for pos in snake_body:
         pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    
-    #code for spawn apple   
+        
     if not apple_spawn:
         apple_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
     apple_spawn = True
     pygame.draw.rect(game_window, red, pygame.Rect(apple_pos[0], apple_pos[1], 10, 10)) 
-    
+
     pygame.display.update()
